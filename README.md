7a8af7f0553b692ad808d359326e0ca28922484c
Kennytian <kt.tian@gmail.com>
2016-08-19 00:31:26 +0800

    example finished

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # react-native-textmore
+
+### Install
+
+`npm install react-native-textmore`
+
+### Usage:
+```
+<TextMore contentStyle={{color :'red' }} contentLines={2} contentText={'如果 v都会指 了 thisArg 参数,或者赋值为 null 或 undefined，则 this 指向全局对象 。'} />
+    
+<TextMore moreText={['Close', 'Open']} contentText={'如果 thisArg 参数有值，则每次 callback 函数被调用的时候，this 都会指向 thisArg 参数上的这个对象。如果省略了 thisArg 参数,或者赋值为 null 或 undefined，则 this 指向全局对象 。'} />
+ 
+<TextMore contentLines={4} contentText={'map 方法会给原数组中的每个元素都按顺序调用一次 callback 函数。callback 每次执行后的返回值组合起来形成一个新数组。 callback 函数只会在有值的索引上被调用；那些从来没被赋过值或者使用 delete 删除的索引则不会被调用。'} />
+```
+
+### Run Example:
+Clone this repository from Github and cd to 'examples' directory then run `npm install`.
+
+### Screenshot:
+![屏幕截图](https://raw.githubusercontent.com/Kennytian/learning-react-native/master/screenshot1.png)
--- index.js
+++ index.js
@@ -1,67 +1,89 @@
-import React, { Component } from 'react';
+import React, {Component, PropTypes} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  TouchableOpacity,
+  Dimensions,
+  View,
   Text,
-  View
+  Platform,
 } from 'react-native';
-import TextMore from './textmore';
 
-export default class test0280 extends Component {
+export default class TextMore extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      contentLines: 0,
+      isShowMore: false,
+      isExpand: false
+    };
   }
 
-  componentWillMount() {
+  static propTypes = {
+    //style:View.propTypes.style, TODO: for view
 
-  }
+    contentText: PropTypes.string.isRequired,
+    contentStyle: Text.propTypes.style,
+    contentLines: PropTypes.number,
+
+    moreText: PropTypes.array.isRequired,
+    moreStyle: Text.propTypes.style
+  };
+
+  static defaultProps = {
+    moreText: ['收起详情', '展开详情'],
+    contentLines: 3
+  };
 
   render() {
+    let moreElement = null;
+    if (this.state.isShowMore) {
+      let {moreText} = this.props;
+      moreElement = (<Text onPress={()=>{this._switchCollapse()}} style={[styles.more_base, {...this.props.moreStyle}]}>{this.state.isExpand ? moreText[0] : moreText[1]}</Text>);
+    }
     return (
-      <View style={styles.container}>
-        <TextMore contentStyle={{ color : 'red' }} contentLines={2}
-                  contentText={'如果 v都会指 了 thisArg 参数,或者赋值为 null 或 undefined，则 this 指向全局对象 。'} />
-        <TextMore moreText={['Close', 'Open']}
-                  contentText={'如果 thisArg 参数有值，则每次 callback 函数被调用的时候，this 都会指向 thisArg 参数上的这个对象。如果省略了 thisArg 参数,或者赋值为 null 或 undefined，则 this 指向全局对象 。'} />
-        <TextMore contentLines={4}
-                  contentText={'map 方法会给原数组中的每个元素都按顺序调用一次 callback 函数。callback 每次执行后的返回值组合起来形成一个新数组。 callback 函数只会在有值的索引上被调用；那些从来没被赋过值或者使用 delete 删除的索引则不会被调用。'} />
-
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <Text>
-          <Text>First part and </Text>
-          <Text>second part</Text>
-        </Text>
+      <View>
+        <Text onLayout={(e)=>{this._onLayout(e)}} style={[styles.content_base, {...this.props.contentStyle}]} numberOfLines={this.state.contentLines}>{this.props.contentText}</Text>
+        {moreElement}
       </View>
-    );
+    )
+  }
+
+  _switchCollapse() {
+    this.setState({
+      isExpand: !this.state.isExpand
+    });
+    if (this.state.contentLines === this.props.contentLines) {
+      this.setState({contentLines: 0})
+    }
+    else {
+      this.setState({contentLines: this.props.contentLines})
+    }
+  }
+
+  _onLayout(e) {
+    let {height} = e.nativeEvent.layout;
+
+    if (height > this.props.contentLines * 24) {
+      if (!this.state.isExpand) {
+        this.setState({contentLines: this.props.contentLines})
+      }
+      if (this.props.contentLines !== 0) {
+        this.setState({isShowMore: true});
+      }
+    }
+
   }
 }
 
-const styles = StyleSheet.create({
-  container    : {
-    flex            : 1,
-    justifyContent  : 'center',
-    alignItems      : 'center',
-    backgroundColor : '#F5FCFF',
+const styles = {
+  content_base: {
+    color: '#666',
+    flex: 1,
+    marginTop: 15,
+    width: Dimensions.get('window').width - 45 - 15,
+    ...Platform.select({ios: {lineHeight: 24}, android: {lineHeight: 34}})
   },
-  welcome      : {
-    fontSize  : 20,
-    textAlign : 'center',
-    margin    : 10,
-  },
-  instructions : {
-    textAlign    : 'center',
-    color        : '#333333',
-    marginBottom : 5,
-  },
-});
+  more_base: {
+    marginTop: 10,
+    fontSize: 14,
+    color: '#1AC964'
+  }
+};
